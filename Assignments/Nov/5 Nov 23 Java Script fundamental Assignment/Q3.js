d89bfa17e8b3ccef25213b0dc11f84c126c193b4
Sudarshan <sudarshan@metamindsystem.com>
2023-12-10 15:32:56 +0530

    done

---
+++ Assignments/Nov/5 Nov 23 Java Script fundamental Assignment/Q3.js
@@ -0,0 +1,6 @@
+const number = 7;
+
+const result = number > 0 ? "positive" : number < 0 ? "negative" : "zero";
+number > 0 ? "positive" : number < 0 ? "negative" : "zero";
+
+console.log(`The number ${number} is ${result}.`);
